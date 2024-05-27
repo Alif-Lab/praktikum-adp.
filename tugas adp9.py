565d1aaeb0d71df606d02981560cca671fb18247
Alif-Lab <75671050+Alif-Lab@users.noreply.github.com>
2024-05-27 20:06:08 +0700

    Add files via upload

---
+++ tugas adp9.py
@@ -0,0 +1,55 @@
+from termcolor import colored, cprint
+import os
+import time
+
+os.system('cls')
+
+x= " "*3
+
+for i in range (4):
+    cprint(x,"red", "on_red",end="  ")
+    
+for i in range (3):
+    cprint(x,"red", "on_red",end="")
+
+print()
+
+for i in range (5):
+    if i < 3 :
+        cprint(x,"red", "on_red",end=" ")
+        print(end=" ")
+        cprint(x,"red", "on_red",end=" ")
+        print(end=" ")
+        cprint(x,"red", "on_red",end=" ")
+        print(end=" ")
+        if i > 0 :
+            cprint(x,"red", "on_dark_grey",end=" ")
+        print(end=" ")
+        if i > 0 :
+            cprint(x,"red", "on_red",end=" ")
+        print()
+        
+        
+    else :
+        cprint(x,"red", "on_red",end=" ")
+        print(end=" ")
+        print(" "*3,end=" ")
+        print(end=" ")
+        cprint(x,"red", "on_red",end=" ")
+        print(end=" ")
+        cprint(x,"red","on_dark_grey",end=" ")
+        print(end=" ")
+        cprint(x,"red", "on_red",end=" ")
+        print()
+        
+    
+
+    
+
+    
+    
+    
+    
+        
+
+
